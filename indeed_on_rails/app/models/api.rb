8e17a21c9c3df8257ab16027aca5ad9ea8b023cf
ZalmanB <zalmanblau@gmail.com>
2015-10-06 19:28:53 -0400

    completed api class

---
+++ indeed_on_rails/app/models/api.rb
@@ -0,0 +1,37 @@
+require 'open-uri'
+
+class Api 
+
+  attr_accessor :user
+  attr_reader :hash
+
+  def call(user_name)
+    @user = User.find_by(name: user_name)
+    fetch
+    binding.pry
+  end
+
+  def fetch
+    @hash = JSON.parse(File.read(open(url)))
+  end
+
+  def url
+    static ||= "http://api.indeed.com/ads/apisearch?publisher=3070567809660386&q="
+    quary ||= "#{languages}&l=#{location}&sort="
+    misc ||= "&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json"
+    "#{static}#{quary}#{misc}"
+  end
+
+  def location 
+    @location ||= user.location.gsub(",", "%2C").gsub(" ", "+")
+  end
+
+  def languages
+    @languages ||= user.languages.map do |lang| 
+      lang.name.gsub("#","%23").gsub("+", "%2B")
+    end
+
+    result ||= @languages.join("%2C+").gsub(" ", "+")
+  end
+
+end
