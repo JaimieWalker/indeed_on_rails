6b0b34cc660383543a8bc66d8f8e9bbd45043deb
JaimieWalker <jaimie.walker90@gmail.com>
2015-10-09 14:07:39 -0400

    Just changed the results we got to 20

--- indeed_on_rails/app/models/api.rb
+++ indeed_on_rails/app/models/api.rb
@@ -44,7 +44,7 @@ class Api
   def api_url
     static ||= "http://api.indeed.com/ads/apisearch?publisher=3070567809660386&q="
     quary ||= "#{languages}&l=#{location}&sort="
-    misc ||= "&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json"
+    misc ||= "&radius=&st=&jt=&start=&limit=20&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json"
     "#{static}#{quary}#{misc}"
   end
 
