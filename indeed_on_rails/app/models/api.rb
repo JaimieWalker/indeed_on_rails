0f10158855abcbbe36d7b033b013e3d735ee941b
ZalmanB <zalmanblau@gmail.com>
2015-10-11 17:33:12 -0400

    faster sometimes.

--- indeed_on_rails/app/models/api.rb
+++ indeed_on_rails/app/models/api.rb
@@ -3,10 +3,15 @@ require 'open-uri'
 class Api 
 
   def self.create_jobs(user_object)
+     
     api = Api.new(user_object)
     api.fetch
 
-    api.hash["results"].each do |job|
+   
+    job_descriptions = Scraper.descriptions(api.hash["results"])
+    
+
+    api.hash["results"].each_with_index do |job, index|
 
       job_hash = {
         job_title: job["jobtitle"],
@@ -14,18 +19,19 @@ class Api
         formatted_location: job["formattedLocation"],
         date: job["date"],
         url: job["url"],
-        description: paragraph = Scraper.description(job["url"])
+        description: job_descriptions[index]
       }
 
       @job = Job.create(job_hash)
 
-      @languages = SkillGap.new(paragraph).find_languages
+      @languages = SkillGap.new(job_descriptions[index]).find_languages
 
       @languages.each do |lang|
         @job.languages << Language.find_or_create_by(name: lang)
       end
 
-      @job.save 
+      @job.save
+    
     end
     
   end
@@ -42,22 +48,22 @@ class Api
   end
 
   def api_url
-    static ||= "http://api.indeed.com/ads/apisearch?publisher=3070567809660386&q="
-    quary ||= "#{languages}&l=#{location}&sort="
-    misc ||= "&radius=&st=&jt=&start=&limit=&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json"
+    static = "http://api.indeed.com/ads/apisearch?publisher=3070567809660386&q="
+    quary = "#{languages}&l=#{location}&sort="
+    misc = "&radius=&st=&jt=&start=&limit=30&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json"
     "#{static}#{quary}#{misc}"
   end
 
   def location 
-    @location ||= user.location.gsub(",", "%2C").gsub(" ", "+")
+    @location = user.location.gsub(",", "%2C").gsub(" ", "+")
   end
 
   def languages
-    @languages ||= user.languages.map do |lang| 
+    @languages = user.languages.map do |lang| 
       lang.name.gsub("#","%23").gsub("+", "%2B")
     end
 
-    result ||= @languages.join("%2C+").gsub(" ", "+")
+    result = @languages.join("%2C+").gsub(" ", "+")
   end
 
 end
--- indeed_on_rails/app/models/scraper.rb
+++ indeed_on_rails/app/models/scraper.rb
@@ -1,8 +1,50 @@
 require 'open-uri'
 class Scraper
 
-  def self.description(url)
-    @doc = Nokogiri::HTML(open(url)).css('table tr td.snip #job_summary').text
+  def initialize(api_jobs)
+    @api_jobs = api_jobs
+    @header = {"Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "Cache-Control" => "no-cache", "Pragma" => "no-cache", "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.168 Safari/535.19"}
+    
+    set_urls
+    make_requests
+  end
+
+  attr_reader :api_jobs, :html_pages, :job_urls, :header
+
+
+  ####################################################################
+  # Returns all the descriptions from the job pages the api provided. 
+  ####################################################################
+
+
+  def self.descriptions(api_jobs)
+    scraper = Scraper.new(api_jobs)
+
+    scraper.html_pages.map do |page|
+      @doc = Nokogiri::HTML(page).css('table tr td.snip #job_summary').text
+    end
+    
+  end
+
+  def set_urls 
+    @job_urls = api_jobs.map {|job| job["url"]}
+  end
+  #####################################################################
+  # Making http request in parrallal with the Typhoeus gem. Significantly reducing lag time. 
+  # Returning the the html response into an array accessible 
+  # to the html_pages method
+  ######################################################################
+
+  def make_requests
+    hydra = Typhoeus::Hydra.new
+    requests = api_jobs.count.times.map do |i|
+      Typhoeus::Request.new(job_urls[i], headers: header, verbose: true, follow_location: true)
+    end
+
+    requests.each { |request| hydra.queue(request) }
+    hydra.run
+
+    @html_pages = requests.map {|job| job.response.response_body}
   end
 
 end
